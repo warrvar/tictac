843fe4f553e05264aac8cd2447ac3ab60df0f945
Hackerpro335 <iurewi4.tix@gmail.com>
2025-03-07 19:36:50 +0300

    I started to add a selector

--- ttt.js
+++ ttt.js
@@ -2,8 +2,42 @@
 var counter = 0;
 var cells = document.querySelectorAll('#field td');
 var header = document.getElementById('header');
+let player = 'X'
+let isPauseGame = false
+let isGameStart = false
 const xPlayerDisplay = document.querySelector('xPlayerDisplay');
+const oPlayerDisplay = document.querySelector('#oPlayerDisplay')
+const restartBtn = document.querySelector('#start-button')
 
+function changePlayer() {
+    player = (player == 'X') ? 'O' : 'X'
+}
+// function randomPick() {
+//     // Pause the game to allow Computer to pick
+//     isPauseGame = true
+
+//     setTimeout(() => {
+//         let randomIndex
+//         do {
+//             // Pick a random index
+//             randomIndex = Math.floor(Math.random() * inputCells.length)
+//         } while (
+//             // Ensure the chosen cell is empty
+//             inputCells[randomIndex] != ''
+//         )
+
+//         // Update the cell with Computer move
+//         updateCell(cells[randomIndex], randomIndex, player)
+//         // Check if Computer not won
+//         if (!checkWinner()) {
+//             changePlayer()
+//             // Swith back to Human player
+//             isPauseGame = false
+//             return
+//         }
+//         player = (player == 'X') ? 'O' : 'X'
+//     }, 1000) // Delay Computer move by 1 second
+// }
 function isVictory() {
     var combos = [
         [0, 1, 2],
@@ -27,11 +61,11 @@ function isVictory() {
 function tap(event) {
     if (counter % 2 == 0) {
         header.innerText = 'It is O turn';
-        event.target.innerHTML = '<img src="melody.png" width=100 bc:transparent>';
+        event.target.innerHTML = '<img src="melody-removebg-preview.png" width=100 bc:transparent>';
     }
     else {
         header.innerText = 'It is X turn';
-        event.target.innerHTML = '<img src="kuromi.png" width=100>';
+        event.target.innerHTML = '<img src="kuromi-removebg-preview.png" width=100>';
     }
 
     if (isVictory()) {
@@ -40,15 +74,20 @@ function tap(event) {
         }
         if (counter % 2 == 0) {
             header.innerText = 'X is winner!';
+            
         }
+        start-button.style.visibility == 'visible';
         else {
             header.innerText = 'O is winner!';
+            
         }
+        start-button.style.visibility == 'visible';
     }
     else if (counter == 8) {
         header.innerText = 'Draw!';
+       
     }
-
+    start-button.style.visibility == 'visible';
     counter++;
     event.target.removeEventListener('click', tap);
 }
